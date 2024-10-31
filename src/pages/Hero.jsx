318b0ea0ae81809b1d9099139cb3555c601c50aa
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-11-01 01:26:48 +0300

    Update Hero.jsx

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -6,6 +6,7 @@ import { swiperData } from "@/constants";
 
 import { Autoplay, Pagination, Navigation } from "swiper/modules";
 import { Button } from "@/components/ui/button";
+import image1 from "@/assets/image1.jpg";
 
 function Hero() {
   return (
@@ -26,11 +27,16 @@ function Hero() {
         className="mySwiper w-full h-auto "
       >
         {swiperData.map((item, index) => (
-          <SwiperSlide key={index}>
-            <div className="w-full h-[70vh] relative">
+          <SwiperSlide key={index} className="relative">
+            <img
+              src={image1}
+              alt="bgImg"
+              className="absolute top-0 left-0 w-full h-full object-cover"
+            />
+            <div className="w-full h-[50vh] lg:h-[70vh] relative">
               <div className="w-full h-full bg-black/80 absolute top-0 left-0 -z-10"></div>
-              <div className="w-full h-full flex items-center justify-center flex-col z-10 lg:px-24 md:px-16 sm:px-6 px-4 bg-black gap-4">
-                <h5 className="lg:text-4xl md:text-3xl sm:text-2xl text-2xl text-indigo-700 font-semibold mb-2 uppercase">
+              <div className="w-full h-full flex items-center justify-center flex-col z-10 lg:px-24 md:px-16 sm:px-6 px-4 gap-4">
+                <h5 className="lg:text-4xl md:text-3xl  text-2xl text-indigo-700 font-semibold mb-2 uppercase">
                   {item.slogan}
                 </h5>
                 <h1 className="lg:text-8xl md:text-7xl text-5xl font-bold text-white mb-4 uppercase">
@@ -39,7 +45,7 @@ function Hero() {
                 <p className="text-base md:text-base lg:text-base text-gray-500 font-medium mb-6 text-center">
                   {item.desc}
                 </p>
-                <Button className="w-[85px] sm:w-[75%] md:w-[35%] lg:w-[15%] text-xl font-semibold rounded-full bg-indigo-600  hover:bg-indigo-600/70 ease-out duration-500">
+                <Button className=" text-xl font-semibold rounded-full bg-indigo-600  hover:bg-indigo-600/70 ease-out duration-500">
                   Get Started
                 </Button>
               </div>
@@ -47,6 +53,15 @@ function Hero() {
           </SwiperSlide>
         ))}
       </Swiper>
+
+      <div className="w-full h-auto flex items-center justify-center md:justify-between sm:justify-center lg:justify-between gap-5 md:gap-7 lg:gap-7 px-6 md:px-16 lg:px-24 mt-6 flex-wrap">
+        <div className="flex flex-col items-center justify-center w-[48%] md:w-[30%] lg:w-fit py-8 lg:p-7 px-10 bg-black/10 hover:bg-black/30  rounded-md ease-out duration-700 cursor-pointer">
+          <h5 className="text-4xl text-indigo-400 font-semibold mb-2 uppercase">
+            1200+
+          </h5>
+          <p className="text-lg text-gray-600 font-medium">Members</p>
+        </div>
+      </div>
     </>
   );
 }
