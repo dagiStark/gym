e7dd0edbf7bd45c69dac9e731439d1bbf4783916
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-11-01 14:59:45 +0300

    adde calculate bmi function

--- src/pages/home/BMI.jsx
+++ src/pages/home/BMI.jsx
@@ -2,8 +2,18 @@ import { Dumbbell } from "lucide-react";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
 function BMI() {
+  const [weight, setWeight] = useState("");
+  const [height, setHeight] = useState("");
+  const [bmi, setBmi] = useState("");
+
+  const calculateBmi = () => {
+    const heightInMeters = height * 0.01;
+    const bmi = weight / (heightInMeters * heightInMeters);
+    setBmi(bmi.toFixed(2));
+  };
   return (
     <div className="w-full h-auto flex items-center py-12 md:py-14 lg:py-16 px-6 md:px-16 lg:px-24">
       <div className="w-full h-auto flex items-center justify-center flex-col">
@@ -17,18 +27,28 @@ function BMI() {
         <div className="w-full h-auto flex items-end justify-center bg-black/20 rounded-md gap-3 md:gap-5 px-2 py-5 md:px-0 flex-wrap">
           <div className="w-[50%] md:w-[25%] h-auto p-2 gap-2 flex flex-col">
             <Label className="">Weight (kg)</Label>
-            <Input placeholder="Enter your weight" />
+            <Input
+              placeholder="Enter your weight"
+              value={weight}
+              onChange={(e) => setWeight(e.target.value)}
+            />
           </div>
           <div className="w-[50%] md:w-[25%] h-auto p-2 gap-2 flex flex-col">
             <Label className="">Height (cm)</Label>
-            <Input placeholder="Enter your height" />
+            <Input
+              placeholder="Enter your height"
+              value={height}
+              onChange={(e) => setHeight(e.target.value)}
+            />
           </div>
           <div className="w-[50%] md:w-[25%] h-auto p-2">
             <Button
+              type="submit"
               variant="outline"
               className="w-full md:w-auto px-7 py-2 text-base font-medium uppercase justify-center text-indigo-600 border border-indigo-500"
+              onClick={calculateBmi}
             >
-              Calculate BMI
+              {bmi ? `Your BMI is ${bmi}` : "Calculate BMI"}
             </Button>
           </div>
         </div>
