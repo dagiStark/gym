cd24963a24d3c3996ab7ae6c4b487554b5609260
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-11-01 16:43:38 +0300

    Update Services.jsx

--- src/pages/home/Services.jsx
+++ src/pages/home/Services.jsx
@@ -1,22 +1,50 @@
 import { Dumbbell } from "lucide-react";
-
+import { servicesData } from "@/constants";
+import { Button } from "@/components/ui/button";
 
 function Services() {
   return (
     <div className="w-full h-auto flex items-center py-12 md:py-14 lg:py-16 px-6 md:px-16 lg:px-24">
-      <div className="w-full h-auto flex items-center justify-center flex-col">
+      <div className="w-full h-auto flex items-center justify-center flex-col gap-3">
         <h6
           className="text-base 
-        md:text-lg font-medium rounded-t-md px-4 py-2 text-gray-200 bg-black/20 flex items-center justify-center md:justify-start gap-x-2 "
+        md:text-lg font-medium rounded-t-md px-4 py-2 text-gray-200 bg-black/20 flex items-center justify-center md:justify-start gap-x-2"
         >
           <Dumbbell className="w-6 h-6 text-indigo-600" />
           Our Services
         </h6>
-        <div className="w-full h-auto flex items-center justify-center gap-x-4 gap-y-5 flex-wrap mb-10">
-          <div className="w-full h-auto rounded-xl bg-black relative overflow-hidden cursor-pointer z-10 hover:-translate-y-">
-            <img src="https://i.ibb.co/4b2r0r3/image.png" alt="bgImg" />
+        <div className="w-full h-auto flex items-start justify-center relative gap-x-4 gap-y-5 flex-wrap mb-10">
+          {servicesData.map((item, index) => (
+            <div
+              key={index}
+              className="w-[20%] rounded-xl bg-black relative overflow-hidden cursor-pointer z-10 hover:-translate-y-10 ease-out transition-all duration-500 py-10"
+            >
+              <div className="w-full h-full bg-black absolute top-0 left-0 -z-10">
+                <img src={item.icon} alt="bgImg" />
+              </div>
 
-          </div>
+              <div className="w-full h-full flex items-center justify-center flex-col text-white p-4 z-50">
+                <div
+                  className="text-base 
+                        md:text-lg font-medium px-4 py-4 text-gray-200 bg-black/20 
+                        flex items-center justify-center md:justify-start gap-x-2 border-4 rounded-full border-indigo-600"
+                >
+                  <Dumbbell className="w-6 h-6 " />
+                </div>
+                <h1 className="text-xl text-gray-100 font-semibold text-center mb-4">
+                  {item.title}
+                </h1>
+                <p className="text-xl text-gray-400 font-normal text-center mb-4">
+                  {item.desc}
+                </p>
+                <div>
+                  <Button className=" text-indigo-600 bg-transparent hover:bg-transparent">
+                    Read More →
+                  </Button>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
