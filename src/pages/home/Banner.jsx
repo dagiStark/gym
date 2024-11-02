d9b237f6e5ef25eb63fff183798c98b9694a570e
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-11-02 22:49:04 +0300

    update banner

--- src/pages/home/Banner.jsx
+++ src/pages/home/Banner.jsx
@@ -1,15 +1,9 @@
 import { Button } from "@/components/ui/button";
-import image5 from "@/assets/images/image5.jpg";
+
 
 function Banner() {
   return (
-    <div className="flex items-center justify-center flex-col py-12 md:py-14 lg:py-16 px-6 md:px-16 lg:px-24 relative z-10">
-      <div className="w-full h-full absolute top-0 left-0 -z-10">
-        <img
-          src="{image5}"
-          alt="bgImg"
-        />
-      </div>
+    <div className="flex items-center justify-center bg-black/20 flex-col py-12 md:py-14 lg:py-16 px-6 md:px-16 lg:px-24 relative z-10">
       <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-gray-200 uppercase mb-4 text-center">
         No <span className="text-indigo-600">Pain</span>, No{" "}
         <span className="text-indigo-600">Gain</span>
