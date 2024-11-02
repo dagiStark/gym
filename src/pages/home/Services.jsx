eced67d8bc2dcba9961f40dbc525641a7c695a4a
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-11-02 22:29:15 +0300

    Update Services.jsx

--- src/pages/home/Services.jsx
+++ src/pages/home/Services.jsx
@@ -5,25 +5,25 @@ import { Button } from "@/components/ui/button";
 function Services() {
   return (
     <div className="w-full h-auto flex items-center py-12 md:py-14 lg:py-16 px-6 md:px-16 lg:px-24">
-      <div className="w-full h-auto flex items-center justify-center flex-col gap-3">
+      <div className="w-full h-auto flex items-center justify-center flex-col gap-3 md:gap-5 lg:gap-6">
         <h6
-          className="text-base 
-        md:text-lg font-medium rounded-t-md px-4 py-2 text-gray-200 bg-black/20 flex items-center justify-center md:justify-start gap-x-2"
+          className="text-2xl md:text-4xl
+         font-medium rounded-t-md px-4 py-2 text-gray-200 bg-black/20 flex items-center justify-center md:justify-start gap-x-2 md:gap-x-4"
         >
-          <Dumbbell className="w-6 h-6 text-indigo-600" />
+          <Dumbbell className="w-6 h-4 md:w-10 md:h-10 -rotate-45 text-indigo-600" />
           Our Services
         </h6>
         <div className="w-full h-auto flex items-start justify-center relative gap-x-4 gap-y-5 flex-wrap mb-10">
           {servicesData.map((item, index) => (
             <div
               key={index}
-              className=" sm:w-[90%] md:w-[40%] lg:w-[20%] rounded-xl bg-black relative overflow-hidden cursor-pointer z-10 hover:-translate-y-10 ease-out transition-all duration-500 py-10"
+              className=" sm:w-[90%] md:w-[40%] lg:w-[20%] md:h-[350px] rounded-xl bg-black relative overflow-hidden cursor-pointer z-10 hover:-translate-y-10 ease-out transition-all duration-500 py-10"
             >
-              <div className="w-full h-full  absolute top-0 left-0 -z-10">
+              <div className="w-full h-full absolute top-0 left-0 -z-10">
                 <img src={item.serviceImg} alt="bgImg" />
               </div>
 
-              <div className="w-full h-full flex items-center justify-center flex-col text-white p-4 z-50">
+              <div className="w-full h-full flex items-center md:absolute top-0 left-0 justify-center flex-col text-white p-4 z-50 md:bg-black md:bg-opacity-60">
                 <div
                   className="text-base 
                         md:text-lg font-medium px-4 py-4 text-gray-200 bg-indigo-200 bg-opacity-30 
